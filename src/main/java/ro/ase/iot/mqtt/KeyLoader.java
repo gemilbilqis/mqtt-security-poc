7c4b1f0c514b70f9b7849481ab49967b22c278da
Bilqis Gemil <gemilbilqis21@stud.ase.ro>
2025-12-11 16:02:59 +0200

    Generated AES key with loading mechanism

--- src/main/java/ro/ase/iot/mqtt/KeyLoader.java
+++ src/main/java/ro/ase/iot/mqtt/KeyLoader.java
@@ -1,4 +1,13 @@
 package ro.ase.iot.mqtt;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 public class KeyLoader {
+    public static byte[] loadKey(String path) throws IOException {
+        String base64 = Files.readString(Paths.get(path)).trim();
+        return Base64.getDecoder().decode(base64);
+    }
 }
